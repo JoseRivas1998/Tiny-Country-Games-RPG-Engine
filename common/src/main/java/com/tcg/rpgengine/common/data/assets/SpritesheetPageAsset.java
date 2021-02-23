3c13e331bd76475385af8b2479024b3b147fefbc
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2021-02-23 10:06:00 -0800

    Spritesheet page

--- common/src/main/java/com/tcg/rpgengine/common/data/assets/SpritesheetPageAsset.java
+++ common/src/main/java/com/tcg/rpgengine/common/data/assets/SpritesheetPageAsset.java
@@ -1,30 +1,97 @@
 package com.tcg.rpgengine.common.data.assets;
 
+import com.tcg.rpgengine.common.data.BinaryDocument;
+import com.tcg.rpgengine.common.utils.UuidUtils;
 import org.json.JSONObject;
 
 import java.nio.ByteBuffer;
+import java.util.Objects;
 import java.util.UUID;
 
-public class SpritesheetPageAsset extends Asset{
+public class SpritesheetPageAsset extends Asset {
 
+    private static final String JSON_PATH_FIELD = "path";
+    private static final String JSON_COLUMNS_FIELD = "columns";
+    private static final String JSON_ROWS_FIELD = "rows";
+    private String path;
+    public int rows;
+    public int columns;
 
-
-    public SpritesheetPageAsset(UUID id) {
+    public SpritesheetPageAsset(UUID id, String path, int rows, int columns) {
         super(id);
+        this.setPath(path);
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public static SpritesheetPageAsset createNewSpritesheetPageAsset(String path, int rows, int columns) {
+        return new SpritesheetPageAsset(UuidUtils.generateUuid(), path, rows, columns);
+    }
+
+    public static SpritesheetPageAsset createFromJSON(String jsonString) {
+        final JSONObject json = new JSONObject(jsonString);
+        final UUID id = UuidUtils.fromString(json.getString(JSON_ID_FIELD));
+        final String path = json.getString(JSON_PATH_FIELD);
+        final int rows = json.getInt(JSON_ROWS_FIELD);
+        final int columns = json.getInt(JSON_COLUMNS_FIELD);
+        return new SpritesheetPageAsset(id, path, rows, columns);
+    }
+
+    public static SpritesheetPageAsset createFromBytes(ByteBuffer bytes) {
+        final UUID id = BinaryDocument.getUuid(bytes);
+        final String path = BinaryDocument.getUTF8String(bytes);
+        final int rows = bytes.getInt();
+        final int columns = bytes.getInt();
+        return new SpritesheetPageAsset(id, path, rows, columns);
     }
 
     @Override
     protected void addAdditionalJSONData(JSONObject jsonObject) {
-
+        jsonObject.put(JSON_PATH_FIELD, this.path);
+        jsonObject.put(JSON_ROWS_FIELD, this.rows);
+        jsonObject.put(JSON_COLUMNS_FIELD, this.columns);
     }
 
     @Override
     protected int contentLength() {
-        return 0;
+        // three integers, one for the length of the string, one for the rows, and one for the columns
+        // Strings are encoded in UTF-8, which is one byte for each character
+        return 3 * Integer.BYTES + this.path.length();
     }
 
     @Override
     protected void encodeContent(ByteBuffer byteBuffer) {
+        BinaryDocument.putUTF8String(byteBuffer, this.path);
+        byteBuffer.putInt(this.rows);
+        byteBuffer.putInt(this.columns);
+    }
 
+    public String getPath() {
+        return this.path;
+    }
+
+    public void setPath(String path) {
+        this.path = Objects.requireNonNull(path);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean result =this == o;
+        if (!result) {
+            if (o == null || this.getClass() != o.getClass()) {
+                result = false;
+            } else if (!super.equals(o)){
+                result = false;
+            } else {
+                SpritesheetPageAsset other = (SpritesheetPageAsset) o;
+                result = this.rows == other.rows && this.columns == other.columns && this.path.equals(other.path);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.path, this.rows, this.columns);
     }
 }
--- common/src/test/java/com/tcg/rpgengine/common/data/assets/SpritesheetPageAssetTest.java
+++ common/src/test/java/com/tcg/rpgengine/common/data/assets/SpritesheetPageAssetTest.java
@@ -1,11 +1,18 @@
 package com.tcg.rpgengine.common.data.assets;
 
+import com.tcg.rpgengine.common.TestUtils;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.mockito.internal.matchers.Null;
+
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
 
 public class SpritesheetPageAssetTest {
 
+    private static final UUID TEST_ID = UUID.fromString("866b1e98-65e8-480b-9fd1-f8068ce08cc5");
     private static final String TEST_PATH = "assets/test_page.png";
     private static final int TEST_ROWS = 2;
     private static final int TEST_COLUMNS = 4;
@@ -35,5 +42,90 @@ public class SpritesheetPageAssetTest {
             // Test Columns (4)
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x04,
     };
+
+    @Test
+    public void canCreateNewSpritesheetPage() {
+        final SpritesheetPageAsset spritesheetPageAsset = SpritesheetPageAsset.createNewSpritesheetPageAsset(
+                TEST_PATH, TEST_ROWS, TEST_COLUMNS
+        );
+        assertNotNull(spritesheetPageAsset);
+        assertEquals(TEST_PATH, spritesheetPageAsset.getPath());
+        assertEquals(TEST_ROWS, spritesheetPageAsset.rows);
+        assertEquals(TEST_COLUMNS, spritesheetPageAsset.columns);
+    }
+
+    @Test
+    public void cannotCreateSpritesheetWithNullPath() {
+        assertThrows(NullPointerException.class, () -> SpritesheetPageAsset.createNewSpritesheetPageAsset(
+                null, TEST_ROWS, TEST_COLUMNS
+        ));
+    }
+
+    @Test
+    public void canSetPathOfSpritesheet() {
+        final SpritesheetPageAsset spritesheetPageAsset = SpritesheetPageAsset.createNewSpritesheetPageAsset(
+                TEST_PATH, TEST_ROWS, TEST_COLUMNS
+        );
+        assertEquals(TEST_PATH, spritesheetPageAsset.getPath());
+        final String expectedPath = "assets/new_path.png";
+        spritesheetPageAsset.setPath(expectedPath);
+        assertEquals(expectedPath, spritesheetPageAsset.getPath());
+    }
+
+    @Test
+    public void cannotSetPathOfSpritesheetToNull() {
+        final SpritesheetPageAsset spritesheetPageAsset = SpritesheetPageAsset.createNewSpritesheetPageAsset(
+                TEST_PATH, TEST_ROWS, TEST_COLUMNS
+        );
+        assertThrows(NullPointerException.class, () -> spritesheetPageAsset.setPath(null));
+    }
+
+    @Test
+    public void verifyJSON() {
+        final SpritesheetPageAsset spritesheetPageAsset = SpritesheetPageAsset.createNewSpritesheetPageAsset(
+                TEST_PATH, TEST_ROWS, TEST_COLUMNS
+        );
+        final JSONObject expectedJSON = new JSONObject(TEST_JSON_STRING);
+        expectedJSON.put("id", spritesheetPageAsset.id.toString());
+        final JSONObject actualJSON = spritesheetPageAsset.toJSON();
+        TestUtils.assertJSONObjectsEquals(expectedJSON, actualJSON);
+    }
+
+    @Test
+    public void canCreateFromJSON() {
+        final SpritesheetPageAsset spritesheetPageAsset = SpritesheetPageAsset.createFromJSON(TEST_JSON_STRING);
+        assertNotNull(spritesheetPageAsset);
+        assertEquals(TEST_ID, spritesheetPageAsset.id);
+        assertEquals(TEST_PATH, spritesheetPageAsset.getPath());
+        assertEquals(TEST_ROWS, spritesheetPageAsset.rows);
+        assertEquals(TEST_COLUMNS, spritesheetPageAsset.columns);
+    }
+
+    @Test
+    public void verifyNumberOfBytes() {
+        final SpritesheetPageAsset spritesheetPageAsset = SpritesheetPageAsset.createFromJSON(TEST_JSON_STRING);
+        final int expectedLength = TEST_BYTES.length;
+        final int actualLength = spritesheetPageAsset.numberOfBytes();
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    public void verifyToBytes() {
+        final SpritesheetPageAsset spritesheetPageAsset = SpritesheetPageAsset.createFromJSON(TEST_JSON_STRING);
+        final byte[] actualBytes = spritesheetPageAsset.toBytes();
+        assertArrayEquals(TEST_BYTES, actualBytes);
+    }
+
+    @Test
+    public void canCreateFromBytes() {
+        final SpritesheetPageAsset spritesheetPageAsset = SpritesheetPageAsset.createFromBytes(
+                ByteBuffer.wrap(TEST_BYTES)
+        );
+        assertNotNull(spritesheetPageAsset);
+        assertEquals(TEST_ID, spritesheetPageAsset.id);
+        assertEquals(TEST_PATH, spritesheetPageAsset.getPath());
+        assertEquals(TEST_ROWS, spritesheetPageAsset.rows);
+        assertEquals(TEST_COLUMNS, spritesheetPageAsset.columns);
+    }
 
 }
